6f50ce92a5671179c2b9423dab05f6d0657db696
D'Artagnan Lamarca <37163261+skaiwolker@users.noreply.github.com>
2022-12-07 14:49:01 -0300

    start front project

---
+++ eCommerceFront/src/app/product/product.ts
@@ -0,0 +1,11 @@
+export interface Product{
+    
+    id: number;
+    name: string;
+    description: string;
+    department: number;
+    userId: number;
+    amount: number;
+    price: number;
+    
+}
